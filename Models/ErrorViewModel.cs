d8ad86e55a2ac455858bc652f330dbb12a0aef74
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-24 21:08:34 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIZJQ7_ASP_2022231.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
