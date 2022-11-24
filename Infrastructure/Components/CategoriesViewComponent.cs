29c148c05ad7c09f3ed104bcf6301786d4d2c0e7
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-24 22:27:05 +0100

    Changews at views.

---
+++ Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIZJQ7_ASP_2022231.Infrastructure.Components
+{
+    public class CategoriesViewComponent:ViewComponent
+    {
+        private readonly ShopContext shopContext;
+        public CategoriesViewComponent(ShopContext shopContext)
+        {
+            this.shopContext = shopContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await shopContext.Categories.ToListAsync());
+    }
+}
