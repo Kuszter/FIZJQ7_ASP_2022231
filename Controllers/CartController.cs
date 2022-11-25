3ee18bdf5a93b2ba12994d1fc0636b201324906f
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-25 15:34:51 +0100

    Cart

---
+++ Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using FIZJQ7_ASP_2022231.Infrastructure;
+using FIZJQ7_ASP_2022231.Models;
+using FIZJQ7_ASP_2022231.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIZJQ7_ASP_2022231.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public CartController(ShopContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem >> ("Cart")??new List<CartItem> ();
+
+            CartViewModel cartvm = new()
+            {
+                cartItems = cart,
+                GrandTotal = cart.Sum(x => x.Quantity * x.Price)
+            };
+
+
+            return View(cartvm);
+        }
+    }
+}
---
+++ Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FIZJQ7_ASP_2022231.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> cartItems { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
