8715762ecb1e83111070e95a1ebe5fd85c154cbc
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-25 11:02:42 +0100

    TagHelpers.cs added .Found on GIthub

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using FIZJQ7_ASP_2022231.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIZJQ7_ASP_2022231.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ShopContext context;
+       public ProductsController(ShopContext context)
+        {
+                this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
