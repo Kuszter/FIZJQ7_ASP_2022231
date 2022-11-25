47c5ade6df377c42692d72bdcbeb6956f35d067f
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-25 14:42:57 +0100

    Cartitem model

---
+++ Models/ViewModels/SmallCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FIZJQ7_ASP_2022231.Models.ViewModels
+{
+    public class SmallCartViewModel
+    {
+        public int NumberOfItems { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
