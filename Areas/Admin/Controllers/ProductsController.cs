0a6daf2b1aa9d01c869cbd0acfe54316ac249c47
Dávid Kusztos <kusztosdavid@gmail.com>
2022-11-25 20:51:51 +0100

    Area made

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using FIZJQ7_ASP_2022231.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIZJQ7_ASP_2022231.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
